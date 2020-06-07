b49ce20347e349456f3f40865ff189df7b94924a
jhonatas <jhonatas.farias@tappsvr.com>
2020-06-07 15:31:25 -0300

    backend (sqlite, knex, migrations, seeds, ts)

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,14 +1,17 @@
 import express from 'express';
+import cors from 'cors'
+import path from 'path';
+import routes from './routes';
 
 const app = express();
 
-app.get('/users', (req, res)=>{
-  return res.json({
-    'nome':'Jhonatas',
-    'sobrenome':'Farias',
-    'carrier':'Front-end',
-    'age':30,
-  });
-})
+app.use(cors({
+  origin:'http://localhost:3000'
+}))
+app.use(express.json())
+
+app.use(routes)
+
+app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')))
 
 app.listen(3333);
