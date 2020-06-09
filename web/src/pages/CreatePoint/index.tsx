9b98df744cb93d86a251b5872d6e8011ce33f264
JhonatasFarias <jhonatas.s.farias@gmail.com>
2020-06-08 22:39:27 -0300

    map

--- web/src/pages/CreatePoint/index.tsx
+++ web/src/pages/CreatePoint/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
 import {FiArrowLeft} from 'react-icons/fi'
+import { Map, TileLayer, Marker } from 'react-leaflet'
 
 import './style.css'
 
@@ -58,6 +59,14 @@ const CreatePoint = () =>{
             <span>Selecione o endereço no mapa</span>
           </legend>
 
+          <Map center={[-23.6611651,-46.6029906]} zoom={15}>
+            <TileLayer
+              attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+              url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            />
+            <Marker position={[-23.6611651,-46.6029906]} />//***************************
+          </Map>
+          
           <div className="field-group">
             <div className="field">
               <label htmlFor="uf">Estado (UF)</label>
