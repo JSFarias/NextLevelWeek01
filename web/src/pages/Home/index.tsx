b12be4269a3d8a7641adef3c24e633f69bd2b962
JhonatasFarias <jhonatas.s.farias@gmail.com>
2020-06-08 22:22:23 -0300

    before react-leaflet

--- web/src/pages/Home/index.tsx
+++ web/src/pages/Home/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
-import {FiLogIn} from 'react-icons/fi'
+import {Link} from 'react-router-dom'
+import {FiLogIn} from 'react-icons/fi' 
 
 import logo from "../../assets/logo.svg"
 import './style.css'
@@ -16,10 +17,10 @@ const Home = ()=>{
           <h1>Seu marketplace de coleta de resíduos.</h1>
           <p>Ajudamos pessoas a encontrarem pontos de coleta de forma eficiente.</p>
 
-          <a href="/cadastro">
+          <Link to="/create-point">
             <span><FiLogIn /></span>
             <strong>Cadastre um ponto de coleta</strong>
-          </a>
+          </Link>
         </main>
 
       </div>
