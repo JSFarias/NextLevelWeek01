9f3e5dffdfab197a1a98523147e1cdfdfee64fdc
JhonatasFarias <jhonatas.farias@tappsvr.com>
2020-06-25 23:20:32 -0300

    routes and nav, before install react-native-maps

---
+++ mobile/src/pages/Points/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import {View, Text} from 'react-native'
+
+const Points = ()=>{
+  return (
+    <>
+      <Text>Points</Text>
+    </>
+  )
+}
+
+export default Points
+
